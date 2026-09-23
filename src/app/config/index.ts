697b8d36bb30b4746efb23646a525fd0bd010bdd
Utsho11 <utshoroy521@gmail.com>
2026-09-23 17:50:56 +0600

    remove demo credentials

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,11 +6,11 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
   NODE_ENV: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 4000,
-  db_url: process.env.MONGODB_URI || process.env.DB_URL || 'mongodb://127.0.0.1:27017/poultrydex',
-  bcrypt_salt_rounds: Number(process.env.BCRYPT_SALT_ROUNDS) || 10,
-  jwt_access_secret: process.env.JWT_SECRET || process.env.JWT_ACCESS_SECRET || 'poultryops_super_secret_jwt_key_2026',
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN || '7d',
-  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET || 'poultryops_super_secret_refresh_jwt_key_2026',
-  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN || '30d',
+  db_url: process.env.MONGODB_URI || process.env.DB_URL ,
+  bcrypt_salt_rounds: Number(process.env.BCRYPT_SALT_ROUNDS) ,
+  jwt_access_secret: process.env.JWT_SECRET || process.env.JWT_ACCESS_SECRET ,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN ,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN ,
   frontend_url: process.env.FRONTEND_URL,
 };
